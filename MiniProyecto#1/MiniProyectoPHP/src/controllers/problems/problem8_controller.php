12b349b00746c0922ae43a42fb1c8d2b70db8c56
Ricardo Gadiel Solis Olave <ricarditogadiel99@gmail.com>
2025-10-17 08:35:30 -0500

    arreglando problema 8

--- MiniProyecto#1/MiniProyectoPHP/src/controllers/problems/problem8_controller.php
+++ MiniProyecto#1/MiniProyectoPHP/src/controllers/problems/problem8_controller.php
@@ -10,18 +10,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $mes = date("m", strtotime($fecha));
     $dia = date("d", strtotime($fecha));
 
-    if (($mes == 3 && $dia >= 21) || ($mes == 4) || ($mes == 5) || ($mes == 6 && $dia <= 20)) {
-        $estacion = "Primavera";
-        $imagen = "primavera.jpg";
-    } elseif (($mes == 6 && $dia >= 21) || ($mes == 7) || ($mes == 8) || ($mes == 9 && $dia <= 20)) {
+    if (($mes == 12 && $dia >= 21) || $mes == 1 || $mes == 2 || ($mes == 3 && $dia <= 20)) {
         $estacion = "Verano";
         $imagen = "verano.jpg";
-    } elseif (($mes == 9 && $dia >= 21) || ($mes == 10) || ($mes == 11) || ($mes == 12 && $dia <= 20)) {
+    } elseif (($mes == 3 && $dia >= 21) || $mes == 4 || $mes == 5 || ($mes == 6 && $dia <= 21)) {
         $estacion = "Otoño";
         $imagen = "otono.jpg";
-    } else {
+    } elseif (($mes == 6 && $dia >= 22) || $mes == 7 || $mes == 8 || ($mes == 9 && $dia <= 22)) {
         $estacion = "Invierno";
         $imagen = "invierno.jpg";
+    } else {
+        $estacion = "Primavera";
+        $imagen = "Primavera.jpg";
     }
 }
 
